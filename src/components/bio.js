17c42e826c020028f95eb6b937f5bdbe96a0fc74
P. Michael Holland <maikeru.horando@gmail.com>
2023-07-27 16:16:12 +0200

    Upgrade to Gatsby 5

--- src/components/bio.js
+++ src/components/bio.js
@@ -2,19 +2,15 @@
  * Bio component that queries for data
  * with Gatsby's useStaticQuery component
  *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
+ * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import React from "react"
+import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      gravatar(email: { eq: "maikeru.horando@gmail.com" }) {
-        url
-      }
       site {
         siteMetadata {
           author {
@@ -22,7 +18,10 @@ const Bio = () => {
             summary
           }
           social {
-            twitter
+            mastodon {
+              name
+              url
+            }
           }
         }
       }
@@ -33,28 +32,18 @@ const Bio = () => {
   const author = data.site.siteMetadata?.author
   const social = data.site.siteMetadata?.social
 
-  const avatar = data?.gravatar.url
-
   return (
     <div className="bio">
-      {avatar && (
-        <Image
-          fixed={{src: avatar}}
-          alt={author?.name || ``}
-          className="bio-avatar"
-          imgStyle={{
-            borderRadius: `50%`,
-          }}
-        />
-      )}
       {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
-        </p>
+        <>
+          <p>
+            Written by <strong>{author.name}</strong> {author?.summary || null}
+          </p>
+          <p>
+            On Mastodon as{" "}
+            <a href={social?.mastodon.url || ``}>{social?.mastodon.name}</a>
+          </p>
+        </>
       )}
     </div>
   )
